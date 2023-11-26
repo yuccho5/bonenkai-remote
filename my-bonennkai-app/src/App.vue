a8c3dc3f0fcd8ea017c5da649ad4d1a4a77ecf9f
yuccho <jirojiro0316@yahoo.co.jp>
2023-11-26 20:07:19 +0900

    コンポーネント修正

--- my-bonennkai-app/src/App.vue
+++ my-bonennkai-app/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <DinosaurAnimation />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DinosaurAnimation from './components/DinosaurAnimation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DinosaurAnimation
   }
 }
 </script>
@@ -24,3 +24,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
--- my-bonennkai-app/src/components/DinosaurAnimation.vue
+++ my-bonennkai-app/src/components/DinosaurAnimation.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="animation-container">
-    <img class="background" src="@/assets/sky-background.png">
+    <!--<img class="background" src="@/assets/sky-background.png"> -->
     <img class="dinosaur" src="@/assets/dinosaur.png">
-    <img class="lower-jaw" v-show="isMouthOpen" src="@/assets/dinosaur-lower-jaw.png">
+    <img class="lower-jaw" src="@/assets/dinosaur-lower-jaw.png">
     <img class="egg" v-show="isEggVisible" :style="eggStyle" src="@/assets/egg.png">
-    <img class="product" v-show="isProductVisible" src="@/assets/product.png">
-    <button v-if="!isAnimationStarted" @click="startAnimation">Start</button>
+    <!--<img class="product" v-show="isProductVisible" src="@/assets/product.png"> -->
+    <button class="start-button" v-if="!isAnimationStarted" @click="startAnimation">ガチャる</button>
   </div>
 </template>
 
@@ -13,7 +13,6 @@
 export default {
   data() {
     return {
-      isMouthOpen: false,
       isEggVisible: false,
       eggStyle: { top: 'initial position' }, // 初期位置
       isProductVisible: false,
@@ -23,13 +22,12 @@ export default {
   methods: {
     startAnimation() {
       this.isAnimationStarted = true;
-      this.isMouthOpen = true;
       setTimeout(() => {
         this.isEggVisible = true;
         this.eggStyle.top = 'falling position'; // 卵が落ちるアニメーション
         setTimeout(() => {
           this.isProductVisible = true; // 商品画像の表示
-        }, 3000); // 卵が落ちてから商品画像が表示されるまでの時間
+        }, 2000); // 卵が落ちてから商品画像が表示されるまでの時間
       }, 2000); // 口が開いてから卵が表示されるまでの時間
     },
     resetAnimation() {
@@ -42,9 +40,45 @@ export default {
 <style scoped>
 .animation-container {
   position: relative;
+  width: 100%; /* コンテナの幅を指定 */
+  height: 100vh; /* コンテナの高さを指定 */
+  overflow: hidden; /* コンテナ外の要素を非表示にする */
 }
-.background, .dinosaur, .lower-jaw, .egg, .product {
+
+.start-button {
+  background-color: red; /* ボタンの背景色 */
+  color: white; /* ボタンのテキスト色 */
+  border: 2px solid gold; /* 金色の縁 */
+  border-radius: 15px; /* 角丸の形状 */
+  padding: 30px 60px; /* ボタン内の余白 */
+  font-size: 40px; /* フォントサイズ */
+  cursor: pointer; /* マウスオーバー時のカーソルをポインタに */
+  transition: background-color 0.3s ease; /* 背景色の変化にトランジションを適用 */
+  position: absolute; /* 絶対位置指定 */
+  left: 15%; /* 画面の左側からの位置 */
+  bottom: 30%; /* 画面の下側からの位置 */
+}
+
+.start-button:hover {
+  background-color: darkred; /* ホバー時の背景色 */
+}
+.dinosaur {
   position: absolute;
+  right: 15%; /* 位置を調整 */
+  bottom: 15%; /* 位置を調整 */
+  width: 60%; /* 画像のサイズを調整 */
 }
-/* 他のCSSスタイル */
+.lower-jaw {
+  position: absolute;
+  right: 20%; /* 位置を調整 */
+  bottom: 60%; /* 位置を調整 */
+  width: 60%; /* 画像のサイズを調整 */
+  transform: translateY(100%); /* 口を適切に位置させる */
+}
+.egg {
+  position: absolute;
+  right: 20%; /* 位置を調整 */
+  bottom: 20%; /* 位置を調整 */
+  width: 10%; /* 画像のサイズを調整 */
+}
 </style>
